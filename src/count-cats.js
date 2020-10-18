864918b6cf552fe776eb270c275fa1961c836262
Arseni Pantsialei <arsenipantsialei@gmail.com>
2020-10-19 01:54:52 +0300

    init: start task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
+module.exports = function countCats(matrix) {
   throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  let count = 0;
+  let cat = '^^';
+
+  for (let i = 0; i < matrix.length; i++) {
+    let currItem = matrix[i];
+    currItem.forEach(item => {
+      if (item == cat) count++
+    })
+  }
+  return count;
+
 };
+//countCats([ [0, 1, '^^'], [0, '^^', 2], ['^^', 1, 2] ]) => 3
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
+module.exports = function createDreamTeam( members ) {
   throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  let teamName = '';
+  for (let i = 0; i < members.length; i++) {
+    let teamItem = members[i];
+    if (typeof(teamItem) === 'string' ) {
+      
+      teamName +=teamItem[0].toUpperCase();
+    }
+  }
+
+  return teamName.split('').sort().join('');
 };
