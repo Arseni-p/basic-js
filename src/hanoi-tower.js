4327fb253c56cbdfa8c3bb956b5610b4c9aba6c0
Arseni Pantsialei <arsenipantsialei@gmail.com>
2020-11-01 23:13:20 +0300

    feat: add hanoi and cipher

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi( disksNumber, turnsSpeed ) {
+  
+  let move = 2 ** disksNumber - 1;
+  let sec = Math.floor(move * 3600 / turnsSpeed);
+
+  let obj = {
+    turns: move,
+    seconds: sec
+  };
+  
+  return obj;
+
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,77 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(modify = true) {
+    this.modify = modify;
+    this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+
+  encrypt(message, key) {
+    if (!(message && key)) throw "Error";
+
+    let newMessage = message.toUpperCase();
+    let newKey = '';
+    let j = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      if (this.alphabet.includes(newMessage[i])) {
+        newKey += key[j % key.length]
+        j++;
+      } else {
+        newKey += message[i];
+      }
+    }
+
+    newKey = newKey.toUpperCase();
+
+    let res = '';
+
+    for (let i = 0; i < message.length; i++) {
+      if (this.alphabet.includes(newMessage[i])) {
+        res += String.fromCharCode(((newMessage.charCodeAt(i) + newKey.charCodeAt(i)) - 130) % 26 + 65);
+      } else {
+        res += newMessage[i];
+      }
+    }
+
+
+    if (this.modify) {
+      return res;
+    } else {
+      return res.split('').reverse().join('');
+    }
+  }
+
+  decrypt(message, key) {
+    if (!(message && key)) throw "Error";
+
+    let newMessage = message.toUpperCase();
+    let newKey = '';
+    let j = 0;
+    for (let i = 0; i < message.length; i++) {
+      if (this.alphabet.includes(newMessage[i])) {
+        newKey += key[j % key.length]
+        j++;
+      } else {
+        newKey += message[i];
+      }
+    }
+
+    newKey = newKey.toUpperCase();
+    let res = '';
+    for (let i = 0; i < message.length; i++) {
+      if (this.alphabet.includes(newMessage[i])) {
+        res += String.fromCharCode((newMessage.charCodeAt(i) + 26 - newKey.charCodeAt(i))  % 26 + 65);
+      } else {
+        res += newMessage[i];
+      }
+    }
+
+    if (this.modify) {
+      return res;
+    } else {
+      return res.split('').reverse().join('');
+    }
   }
 }
 
